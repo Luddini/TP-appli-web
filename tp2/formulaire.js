c0700da1539c4a809b5a056ee808fc0974260e29
Luddini <72726930+Luddini@users.noreply.github.com>
2020-11-03 13:40:42 +0100

    Add files via upload

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -2,7 +2,7 @@ function validation(){
   
     var nom = document.getElementById("Nom").value;
     
-   
+  
     document.getElementById("error").innerHTML = "";
   
     for(let item of document.querySelectorAll('.inputAW')){
@@ -10,7 +10,7 @@ function validation(){
         // recuperation du label
         var label =   document.querySelector("label[for="+item.id+"]").textContent;
         // ajout du message d'erreur
-        document.getElementById("error").innerHTML += `Minimum 5 caractères pour : ${label} <br/>`;
+        document.getElementById("error").innerHTML += `La saisie de ${label} est obligatoire <br/>`;
         
         // ajout de la classe CSS "display"
         document.getElementById("error").classList.add("display");
@@ -26,11 +26,10 @@ function validation(){
         document.getElementById("Resultat").classList.add("display")
       }
 
-      if(nom.length == 0){
+      if(nom == null){
       
-        document.getElementById("error").innerHTML = `La saisie du nom est obligatoire`;
-        document.getElementById("error").classList.remove("display")
-        document.getElementById("Resultat").classList.add("display")
+        document.getElementById("error").innerHTML += "La saisie du nom est obligatoire";
+      
     }
     
 }
